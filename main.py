da62133172dddb5eb8a2cc7918f90787b3554ab2
KenjiDoom <vitodevcool@protonmail.com>
2021-04-12 02:06:08 +0000

    User input are allowed now

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ def main():
             """)
 
     passfile = input("> ")
+    basedir = input("> ")
+    mountdir = input("> ")
     fileopen = open(passfile,  'r') # rb for bytes inlcuded
 
     for password in fileopen: # This will give it the loop
         passwords = password.strip() # Now we need a destination for the passwords
-        command1 = pexpect.spawn('cryfs /home/%USER/.local/share/plasma-vault/test_today.enc/ /home/%USER/Vaults/test_today')
+        #command1 = pexpect.spawn('cryfs /home/%USER/.local/share/plasma-vault/test_today.enc/ /home/%USER/Vaults/test_today')
+        command1.pexpect.spawn('cryfs {0} {1}'.format(basedir, mountdir))
         command1.expect('Password:')
         command1.sendline(password)
         result = command1.expect(['Error 11: Could not load config file. Did you enter the correct password?', 'Mounting filesystem. To unmount, call:'])
